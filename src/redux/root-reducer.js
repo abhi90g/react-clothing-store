36111cf26cbfbd38fe9e250e2b620c6d2ddb8494
Abhinav Gupta <abhinavgupta@Abhinavs-MacBook-Pro.local>
2019-09-08 18:08:55 -0700

    created shop reducer and organized code

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,24 @@
 import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import userReducer from '../redux/user/user.reducer'
 import cartReducer from '../redux/cart/cart.reducer'
+import directoryReducer from '../redux/directory/directory.reducer'
+import shopReducer from '../redux/shop/shop.reducer'
 
-export default combineReducers ({
+// config for redux-presist to store cart in local storage
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers ({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer,
+    shop: shopReducer
 })
+
+export default persistReducer(persistConfig, rootReducer)
